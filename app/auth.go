3322ee2a34dc6f4f20c057f06534891ea74eb020
hei <32mens@gmail.com>
2016-08-22 15:20:52 +0900

    add comment

--- app/auth.go
+++ app/auth.go
@@ -13,6 +13,17 @@ type UserClaim struct {
 	jwt.StandardClaims
 }
 
+/*
+	This API server use JWT(JSON WEB TOKEN). it is the login status save to hashed token and
+	client use token to authorize user infomation.
+	This API Server create salt for each user and save Salt table in MySQL,
+	so JWT authorization step is
+
+	1. Get user_id from Token, and get salt correspond to user from database table
+	2. JWT validation in using salt. check to chenge from other user
+
+*/
+
 func (l Login) Auth() (int, error) {
 	pt, err := jwt.Parse(l.Token, func(token *jwt.Token) (interface{}, error) {
 
@@ -79,6 +90,7 @@ func (l *Login) Create(b []byte) error {
 		return errors.Wrapf(err, "User salt not exist: user_id: %v", i)
 	}
 
+	// create one day token
 	claim := UserClaim{
 		i,
 		jwt.StandardClaims{
